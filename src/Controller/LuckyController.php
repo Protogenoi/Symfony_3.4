d7ea752fcbf4dcadd86573762b6800e5344f83af
Michael <michael@adl-crm.uk>
2018-06-26 17:21:21 +0100

    Changed to Annotation Routes and Rendering a Template with Twig

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -3,15 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+ use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyController
+class LuckyController extends Controller
 {
+    /**
+     * @Route("/lucky/number")
+     */
     public function number()
     {
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('lucky/number.html.twig', array(
+            'number' => $number,
+        ));
     }
 }
